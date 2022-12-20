1487c70fa80ea7a82d4f978a78890616c5c9b03b
Tate <tatebraeckel@gmail.com>
2022-12-20 12:26:39 -0700

    commit tree c8f8f42a85fbf64b17d1d6b176205936f20aa198

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -9,15 +9,17 @@ function ColorList() {
     "hotpink",
   ];
 
+  const colorCard = ColorList.map((color) => {
+    return (
+      <li style={{ color }}>{colors}</li>
+    );
+  })
+
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {colorCard}
       </ol>
     </div>
   );
